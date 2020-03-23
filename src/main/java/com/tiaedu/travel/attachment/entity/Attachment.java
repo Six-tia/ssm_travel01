4098eb422dfce156657d6dc6e2bb4314d7dc02f1
Six-tia <tianyi.666tia@gmail.com>
2020-03-23 19:58:12 +1100

    Initial upload with basic directory structure and an entity of attachment.

---
+++ src/main/java/com/tiaedu/travel/attachment/entity/Attachment.java
@@ -0,0 +1,116 @@
+package com.tiaedu.travel.attachment.entity;
+
+import java.util.Date;
+
+//附件文件实体类
+public class Attachment {
+
+    private Integer id;
+    private String title;
+    private String fileName;
+    private String contentType;
+    private String filePath;
+    private String fileDigest;
+    private Integer athType; //附件归属类型
+    private Integer belongId; //附件归属对象id
+    private String createdUser;
+    private Date createdTime;
+    private String modifiedUser;
+    private Date modifiedTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileDigest() {
+        return fileDigest;
+    }
+
+    public void setFileDigest(String fileDigest) {
+        this.fileDigest = fileDigest;
+    }
+
+    public Integer getAthType() {
+        return athType;
+    }
+
+    public void setAthType(Integer athType) {
+        this.athType = athType;
+    }
+
+    public Integer getBelongId() {
+        return belongId;
+    }
+
+    public void setBelongId(Integer belongId) {
+        this.belongId = belongId;
+    }
+
+    public String getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(String createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getModifiedUser() {
+        return modifiedUser;
+    }
+
+    public void setModifiedUser(String modifiedUser) {
+        this.modifiedUser = modifiedUser;
+    }
+
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+}
